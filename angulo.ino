d0203290d8d277dbce0b74a0e3c093ff8f7db236
Ancroa <38019141+Ancroa@users.noreply.github.com>
2018-04-11 11:34:49 -0300

    Update angulo.ino

--- angulo.ino
+++ angulo.ino
@@ -27,14 +27,14 @@
 Adafruit_BNO055 bnoA = Adafruit_BNO055(-1, BNO055_ADDRESS_A);
 Adafruit_BNO055 bnoB = Adafruit_BNO055(-1, BNO055_ADDRESS_B);
 
-SoftwareSerial xbee(2, 3); //Arduino RX, TX
+SoftwareSerial xbee(2, 3);                //Arduino RX, TX
 
 String dados;
 
 void setup(void)
 {
   Serial.begin(9600);
-  //
+
   /* Verificando se o sensor está conectado */
   if (!bnoA.begin()) {
     Serial.print("BNO055(A) GND não encontrado");
@@ -48,8 +48,7 @@ void setup(void)
   }
   bnoB.setExtCrystalUse(true);
 
-
-  // set the data rate for the SoftwareSerial port
+   /* Setado o BaudRate do Xbee */
   xbee.begin(9600);
 
   delay(1000);
@@ -57,6 +56,8 @@ void setup(void)
 
 void loop(void)
 {
+   
+   /* Verificando se os sensores estão calibrados */
   uint8_t system, gyroA, accelA, magA, gyroB, accelB, magB;
   system = gyroA = gyroB = 0;
   bnoA.getCalibration(&system, &gyroA, &accelA, &magA);
@@ -67,19 +68,26 @@ void loop(void)
   //  Serial.print("B: ");
   //  Serial.println(gyroB);
 
+   /* Condição de inicialização da leitura dos sensores */
   if (gyroA == gyroB && gyroA == 3) {
+     
     imu::Vector<3> euler = bnoA.getVector(Adafruit_BNO055::VECTOR_EULER);
     dados = dados + euler.x() + " " + euler.y() + " "  + euler.z() + " ";
 
     euler = bnoB.getVector(Adafruit_BNO055::VECTOR_EULER);
     dados = dados + euler.x() + " " + euler.y() + " "  + euler.z() + ";";
 
+    /* Ex. de organização dos dados */
+    /* 50.00 35.14 154.95 0.00 359.51 275.01; */
+     
+     /* Envia o dados pelo xbee */
     xbee.print(dados);
 
+     /* Aprensenta na tela o valor enviado */
     Serial.println(dados);
 
-    dados = "";
-    xbee.flush();
+    dados = "";         // Limpa a string dados
+    xbee.flush();       //Limpa a serial do xbee
   }
   delay(100);
 }
