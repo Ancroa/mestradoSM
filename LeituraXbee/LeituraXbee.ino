82562ad285e8ccd2592557c0506155e540c624ce
Ancroa <38019141+Ancroa@users.noreply.github.com>
2018-04-16 12:25:59 -0300

    Add files via upload

---
+++ LeituraXbee/LeituraXbee.ino
@@ -0,0 +1,71 @@
+/* Conexão dos pinos do xbee ao arduino
+   ===========
+   Conecte o TX ao D3
+   Conecte o RX ao D2
+   Conecte Vgg ao 5V
+   Conecte GROUND ao GND
+*/
+
+/* Conexão dos pinos do sensor BNO055
+   ===========
+   Conecte o SCL ao A5
+   Conecte o SDA ao A4
+   Conecte VDD ao 3.3V
+   Conecte GROUND ao GND
+   Conecte ADR ao GND para o endereço 0x28 (A)
+   Conecte ADR ao 3.3V para o endereço 0x29 (B)
+*/
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial xbee(2, 3);                // Arduino RX, TX
+int i = 0;                                // Quantidade de dados recebido pelo xbee
+int k = 0;                                // Índice do vetor valor
+char valor[7];                            // Armazena o valor dos dados recebido
+
+void setup(void)
+{
+  Serial.begin(9600);                     // BaudRate da Serial
+  xbee.begin(9600);                       // BaudRate do xbee
+
+  delay(100);
+}
+
+
+
+void loop(void)
+{
+  if (xbee.available() > 0) {             // Verificação de dados recebidos
+
+    byte frame = xbee.read();             // Armazena o primeiro dado do pacote
+    i++;                                  // Conta a quantidade de dados recebido do pacote
+
+    if (i > 15 && frame != 0xC2) {        // Condição de inicialização dos dados armazenado no pacote
+
+      char j = (char)frame;               // Converter o dado recebido em char
+
+      if (j == ' ' || j == ';') {         // Verifica a condição para realizar a conversão dos dados para inteiro
+        k = 0;                            // Zera o índice do vetor de caracter
+        String str(valor);                // Converter o vetor de caracter em string ... Ex. [1] [0] [0] [.] [5] [2] - > 100.52
+        Serial.print(atof(str.c_str()));  // Converte a String em número
+        Serial.print("\t");
+
+        // Limpa o vetor de caracter
+        valor[0] = '\0'; valor[3] = '\0'; valor[6] = '\0';
+        valor[1] = '\0'; valor[4] = '\0';
+        valor[2] = '\0'; valor[5] = '\0';
+
+        if (j == ';') {                   // Condição para receber um novo pacote
+          Serial.print("\n");
+          frame = 0x00;                   // Limpa o frame
+          i = 0;                          // Zera a quantidade de dados do pacote recebido
+        }
+
+      }
+      else {                              // Caso a condição não seja verdadeira, armazena o caracter no vetor
+        valor[k] = j;                     // Guarda o caracter no vetor de caracteres
+        k++;                              // Incrementa o índice do vetor
+      }
+    }
+  }
+}
---
+++ Serial_Arduino/Serial_Arduino.m
@@ -0,0 +1,291 @@
+function varargout = Serial_Arduino(varargin)
+% SERIAL_ARDUINO MATLAB code for Serial_Arduino.fig
+%      SERIAL_ARDUINO, by itself, creates a new SERIAL_ARDUINO or raises the existing
+%      singleton*.
+%
+%      H = SERIAL_ARDUINO returns the handle to a new SERIAL_ARDUINO or the handle to
+%      the existing singleton*.
+%
+%      SERIAL_ARDUINO('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in SERIAL_ARDUINO.M with the given input arguments.
+%
+%      SERIAL_ARDUINO('Property','Value',...) creates a new SERIAL_ARDUINO or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before Serial_Arduino_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to Serial_Arduino_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help Serial_Arduino
+
+% Last Modified by GUIDE v2.5 16-Apr-2018 10:02:40
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+    'gui_Singleton',  gui_Singleton, ...
+    'gui_OpeningFcn', @Serial_Arduino_OpeningFcn, ...
+    'gui_OutputFcn',  @Serial_Arduino_OutputFcn, ...
+    'gui_LayoutFcn',  [] , ...
+    'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before Serial_Arduino is made visible.
+function Serial_Arduino_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to Serial_Arduino (see VARARGIN)
+
+% Choose default command line output for Serial_Arduino
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes Serial_Arduino wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = Serial_Arduino_OutputFcn(hObject, eventdata, handles)
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in Parar.
+function Parar_Callback(hObject, eventdata, handles)
+% Quando o botão Parar Leitura for precionado, então o valor stop_now passa a ser 1
+handles.stop_now = 1;
+% Atualiza o gui data
+guidata(hObject, handles);
+
+% --- Executes on button press in A_x.
+function A_x_Callback(hObject, eventdata, handles)
+% hObject    handle to A_x (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+global matriz_dados
+
+% Atualizando o GUI data
+guidata(hObject,handles);
+
+if(handles.stop_now == 1)
+    plot(handles.axes1,matriz_dados(:,1),'r');
+end
+
+% --- Executes on button press in A_y.
+function A_y_Callback(hObject, eventdata, handles)
+% hObject    handle to A_y (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global matriz_dados
+
+% Atualizando o GUI data
+guidata(hObject,handles);
+
+if(handles.stop_now == 1)
+    plot(handles.axes1,matriz_dados(:,2),'b');
+end
+
+% --- Executes on button press in A_z.
+function A_z_Callback(hObject, eventdata, handles)
+% hObject    handle to A_z (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global matriz_dados
+
+% Atualizando o GUI data
+guidata(hObject,handles);
+
+if(handles.stop_now == 1)
+    
+    
+    plot(handles.axes1,matriz_dados(:,3),'g');
+end
+
+% --- Executes on button press in B_x.
+function B_x_Callback(hObject, eventdata, handles)
+% hObject    handle to B_x (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global matriz_dados
+
+% Atualizando o GUI data
+guidata(hObject,handles);
+
+if(handles.stop_now == 1)
+    
+    plot(handles.axes1,matriz_dados(:,4),'m');
+end
+
+% --- Executes on button press in B_y.
+function B_y_Callback(hObject, eventdata, handles)
+% hObject    handle to B_y (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global matriz_dados
+
+% Atualizando o GUI data
+guidata(hObject,handles);
+
+if(handles.stop_now == 1)
+    plot(handles.axes1,matriz_dados(:,5),'c');
+end
+
+% --- Executes on button press in B_z.
+function B_z_Callback(hObject, eventdata, handles)
+% hObject    handle to B_z (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global matriz_dados
+
+% Atualizando o GUI data
+guidata(hObject,handles);
+
+if(handles.stop_now == 1)
+    plot(handles.axes1,matriz_dados(:,6),'k');
+end
+
+% --- Executes on button press in Iniciar.
+function Iniciar_Callback(hObject, eventdata, handles)
+% variável de armazenamento da serial
+global s
+global matriz_dados
+
+% Busca pela serial do Arduino
+Numero_da_Porta=1;
+Porta_Conectada=1;
+
+% Enquanto a porta não for conectada permanece no while
+while Porta_Conectada
+    s = serial(['COM' num2str(Numero_da_Porta)]);
+    try
+        % Setando os parâmetros da Serial
+        set(s, 'InputBufferSize', 128); %number of bytes in inout buffer
+        set(s, 'FlowControl', 'none');
+        set(s, 'BaudRate', 9600);
+        set(s, 'Parity', 'none');
+        set(s, 'DataBits', 8);
+        set(s, 'StopBit', 1);
+        set(s, 'Timeout',100);
+        
+        % Abrindo porta Serial
+        fopen(s);
+        
+        % Condição de parada do while (Porta conectada)
+        Porta_Conectada=0;
+        
+        % Inicializando o botão de parada
+        handles.stop_now = 0;
+        
+        % Atualizando o GUI data
+        guidata(hObject,handles);
+        
+        % Abrindo o arquivo txt para armazenar os dados
+        fileID = fopen('dados_Vanessa.txt','w');
+        
+        % Escrevendo o cabeçalho no arquivo txt
+        fprintf(fileID,'%s\t%s\t%s\t%s\t%s\t%s\n','Ax','Ay','Az','Bx','By','Bz');
+        
+        disp('Conectado!');
+        % Condição de armazenamento dos dados no arquivo txt. Enquanto o botão de parada
+        % não for acionado, permanece no while
+        %figure
+        hold on
+        while ~(handles.stop_now)
+            % Atualiza o gui data
+            
+            drawnow();
+            handles = guidata(hObject);
+            
+            % Se houver alguma coisa na serial a condição é verdadeira
+            if s.BytesAvailable > 0
+                % Armazena os dados na variável data
+                data = fscanf(s);
+                
+                % Grava os dados no arquivo txt aberto
+                fprintf(fileID,'%s\n',data);
+                matriz_dados = dlmread('dados_Vanessa.txt','',1,0);
+                
+                %%%%% Gráfico do sensor A (Coxa)
+                if get(handles.A_x, 'Value') == 1
+                    plot(handles.axes1,matriz_dados(:,1),'r');
+                end
+                
+                if get(handles.A_y, 'Value') == 1
+                    plot(handles.axes1,matriz_dados(:,2),'b');
+                end
+                
+                if get(handles.A_z, 'Value') == 1
+                    plot(handles.axes1,matriz_dados(:,3),'g');
+                end
+                
+                %%%%% Gráfico do sensor B (Panturrilha)
+                if get(handles.B_x, 'Value') == 1
+                    plot(handles.axes1,matriz_dados(:,4),'m');
+                end
+                
+                if get(handles.B_y, 'Value') == 1
+                    plot(handles.axes1,matriz_dados(:,5),'c');
+                end
+                
+                if get(handles.B_z, 'Value') == 1
+                    plot(handles.axes1,matriz_dados(:,6),'k');
+                end
+                
+                % Plota em tempo real o gráfico dos ângulos dos sensores
+                
+            end
+        end
+        
+        disp('Parou');
+        
+        % Armazena os dados do txt numa matriz para avalia-los
+        %matriz_dados = dlmread('Local_de_armazenamento_dos_dados.txt','',1,0);
+        
+    catch
+        % deleta a porta serial quando terminar o processo
+        delete(s);
+        
+        % limpa a porta serialSSSS
+        clear s
+        
+        % desconecta todas as portas abertas
+        instrreset % close any wrongly opened connection
+        
+        % condição para iniciar o while da porta serial não conectada
+        Porta_Conectada =1 ; % keep trying...
+        
+        disp('NaoFoiConectado')
+    end
+    
+    % incremento do contador da porta serial
+    Numero_da_Porta = Numero_da_Porta + 1
+    
+    % Se o número de porta serial for igual a 16, para o processo de busca da serial
+    if Numero_da_Porta == 16
+        error('Arduino is not connected')
+    end
+end
